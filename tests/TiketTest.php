832bb18bc0caf632bab1aaccfbfb9622611acb3e
Teguh Muhammad <mteguhasuandi@gmail.com>
2015-10-06 10:17:21 +0700

    Test TiketTest.php using PHPUnit

--- tests/TiketTest.php
+++ tests/TiketTest.php
@@ -4,11 +4,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Mtasuandi\Tiket\Auth\TiketAuth;
 use Mtasuandi\Tiket\Exceptions\AuthException;
 
-$auth = new TiketAuth( 'f5c0502333aeedebb9a4f70aa8b234de' );
-
-try {
-	$access_token = $auth->getToken();
-	echo '<pre>'; print_r( $access_token );
-} catch (AuthException $e) {
-	echo $e->getMessage();
+class TiketTest extends PHPUnit_Framework_TestCase {
+	public function testGetToken()
+	{
+		$auth = new TiketAuth( 'f5c0502333aeedebb9a4f70aa8b234de' );
+		$access_token = $auth->getToken();
+		$this->assertContains('token', $access_token);
+		$this->assertArrayHasKey('diagnostic', ['diagnostic' => []]);
+	}
 }
